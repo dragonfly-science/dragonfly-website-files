43afd5d5a196d751ee16bc5ef7809af76da828dc
Richard Mansfield <richard@dragonfly.co.nz>
2022-11-01 09:00:22 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cat > ${HOME}/.aws/credentials << EOF
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOF
-s3 sync files/ s3://files.dragonfly.co.nz/ --acl public-read
+aws s3 sync files/ s3://files.dragonfly.co.nz/ --acl public-read
