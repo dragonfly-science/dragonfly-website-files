5075b9e1bb502d05ceba49c470568b542728bec4
Richard Mansfield <richard@dragonfly.co.nz>
2022-11-01 08:57:18 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+mkdir -p ${HOME}/.aws
+cat > ${HOME}/.aws/credentials << EOF
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOF
 s3 sync files/ s3://files.dragonfly.co.nz/ --acl public-read
